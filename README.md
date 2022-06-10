087f4095407053c65acf9a533b44a8ca78c7cdda
Kaleb Tsegaye <kalebts@gmail.com>
2022-06-10 01:04:29 -0400

    streamlit, other changes

--- README.md
+++ README.md
@@ -6,13 +6,67 @@ Synthesizers, or synths, are music instruments that create sounds using electric
 
 Synthesizers have had a long history being developed as hardware instruments, but now they are available to be used through software, meaning they are more accessible. Synthesizers are a commonly used instrument, due to their versatility and portability. With the advent of increased accesibility comes more music makers/enthusiasts who are curious about the creation of sounds they may hear elsewhere. One community where discussion about this topic occurs is the Reddit forum, [/r/SynthRecipes](www.reddit.com/r/synthrecipes). On this forum, users come with questions about recreating the synths they hear in music, and other users reply with suggestions or solutions to recreating the synth. While this forum is helpful, in the case that someone well-versed in sound synthesis is not available, or the sound is too complicated, an alternative way may be a better option.
 
-### Objective
+## Objective
 
 The purpose of this project is to use neural networks to estimate the parameters of the oscillators that make up synth sound. This will be done using a recreation of a synthesizer in Python by [18alantom](https://github.com/18alantom/synth). Audio files of a synth playing one note will be input for the model to produce estimations. Spectrograms of the audio files will be used to represent the audio frequencies present in the audio file. These will be used in a Convolutional Neural Network model.
 
 The model will be trained with audio files generated randomly using a Python script and tested with files from the same batch of files.
 
 To gauge how successful the estimations are, I will take into account the waveforms that are predicted and the frequency/phase of the waves. If time permits, I would like to incorporate more complicated audio files for the estimation of Attack Delay Sustain Release (ADSR) and filter parameters.
+
+## Repository Structure
+
+📂 audio_files
+    -> 📂 1_basic_waves - .wav/.png/.csv files for first model
+    -> 📂 2_basic_waves_adsr - .wav/.png/.csv files for second group of models
+    -> 📂 3_mult_waves_detune_adsr - .wav/.png/.csv files for third group of models
+    -> 📂 archive - .wav/.png files that were not used
+    -> 📂 one_offs - .wav/.csv files made for testing
+
+📂 code - containing Jupyter notebooks where I contructed my models
+
+📂 components - portions of 18alantom's synth repo, some of it altered for my project
+
+📂 files - various individual files
+    -> 📄 generating_samples.py - script for creating randomized audio samples to train/test models
+    -> 📄 requirements.txt - required packages
+
+📂 models - saved Tensorflow models
+
+📂 synth - 18alantom's original synth repo
+
+📄 streamlit-app.py - Streamlit app
+
+## Essential Libraries
+
+### Handling/Modifying data
+
+* Pandas (working with .csv data)
+
+* Numpy (used throughout different packages)
+
+* Scipy (loading/writing .wav files)
+
+### Modeling/Analysis
+
+* Tensorflow (waveform/parameter estimation)
+
+* Sewar* (spectrogram comparison)
+
+* CREPE* (pitch detection)
+
+### Visualizations
+
+* Matplotlib (spectrogram, amplitude plots)
+
+* librosa (amplitude plots)
+
+### Streamlit
+
+### [Synth Code from 18alantom](https://github.com/18alantom/synth)
+
+* not on Anaconda    
+
 
 
 Image Source: Omegatron, *Wikimedia*, [Link](https://commons.wikimedia.org/wiki/File:Waveforms.svg), [Creative Commons License](https://creativecommons.org/licenses/by-sa/3.0/)
--- streamlit-app.py
+++ streamlit-app.py
@@ -1,3 +1,4 @@
+from random import sample
 import streamlit as st
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -9,12 +10,21 @@ from scipy.io import wavfile
 import h5py
 import json
 import re
+from librosa import display
+import math
+import crepe
 
 from components import composers, envelopes, modifiers, oscillators
 
 st.set_page_config(layout="wide") # make app wide
 
+SR = 44100 # sample rate
+
+# wav helper functions
 to_16 = lambda wav, amp: np.int16(wav * amp * (2**15 - 1))
+
+def get_val(osc, sample_rate=SR):
+    return [next(osc) for i in range(sample_rate)]
 
 def wave_to_file(wav, wav2=None, fname="temp.wav", amp=0.1):
     wav = np.array(wav)
@@ -24,9 +34,10 @@ def wave_to_file(wav, wav2=None, fname="temp.wav", amp=0.1):
         wav2 = to_16(wav2, amp)
         wav = np.stack([wav, wav2]).T
     
-    wavfile.write(fname, 44100, wav)
+    wavfile.write(fname, SR, wav)
 
 # helper function for loading models with class labels
+# code from https://stackoverflow.com/questions/44310448/attaching-class-labels-to-a-keras-model
 def load_model_ext(filepath, custom_objects=None):
     model = tf.keras.models.load_model(filepath, custom_objects=None)
     f = h5py.File(filepath, mode='r')
@@ -45,51 +56,76 @@ def load_models():
 
 wv_model, class_labels, adsr_model = load_models()
 
-st.title('Synth Parameter Estimator 🎹')
-
-file = st.file_uploader('Upload an audio file (.wav please)', type=['.wav', '.mp3', '.m4a', '.flac'], help='audio file to analyze')
-
-if file is not None:
-    sample_rate, samples = wavfile.read(file)
-    
+def make_plots(samples, sample_rate, file_name):
     fig = plt.Figure()
     ax = fig.add_subplot(111)
     ax.specgram(samples, Fs=sample_rate, cmap="plasma", NFFT=256, mode='default')
     canvas = FigureCanvas(fig)
-    canvas.print_figure(f'spec_axis.png', bbox_inches='tight')
+    canvas.print_figure(f'{file_name}_spec_axis.png', bbox_inches='tight')
     ax.axis('off')
     canvas = FigureCanvas(fig)
-    canvas.print_figure(f'spec_no_axis.png', bbox_inches='tight')
+    canvas.print_figure(f'{file_name}_spec_no_axis.png', bbox_inches='tight')
 
-    with plt.ioff():
-        fig = plt.Figure()
-        ax = fig.add_subplot(111)
-        ax.plot(samples)
-        canvas = FigureCanvas(fig)
-        canvas.print_figure(f'adsr_axis.png', bbox_inches='tight')
-        ax.axis('off')
-        canvas = FigureCanvas(fig)
-        canvas.print_figure(f'adsr_no_axis.png', bbox_inches='tight')
+    fig = plt.Figure()
+    ax = fig.add_subplot(111)
+    ax.plot(samples)
+    canvas = FigureCanvas(fig)
+    canvas.print_figure(f'{file_name}_adsr_axis.png', bbox_inches='tight')
+    ax.axis('off')
+    canvas = FigureCanvas(fig)
+    canvas.print_figure(f'{file_name}_adsr_no_axis.png', bbox_inches='tight')
 
-    col1, col2 = st.columns((5,5))
+    return None
 
-    with col1:
-        st.image(load_img('spec_axis.png'), caption='Spectrogram')
-    with col2:
-        st.image(load_img('adsr_axis.png'), caption='Amplitude')
+st.title('Synth Parameter Estimator 🎹')
+
+upload_columns = st.columns((5,5))
+with upload_columns[0]:
+    st.text('Pitch detection (courtesy of CREPE) is in use.')
+    predict_note_octave = st.checkbox('Would you rather select the note and octave?')
+    if predict_note_octave == True:
+        chosen_note = st.selectbox('Pick note', options=['A', 'A#', 'B', 'C', 'C#', 'D', 'E', 'F', 'F#', 'G', 'G#'])
+        chosen_octave = st.number_input('Pick octave', min_value=0, max_value=9)
+with upload_columns[1]:
+    file = st.file_uploader('Upload an audio file (.wav please)', type=['.wav'], help='audio file to analyze')
+
+# '.mp3', '.m4a', '.flac'
+
+if file is not None:
+    sample_rate, samples = wavfile.read(file)
+    lib_samp, lib_sample_rate = librosa.load(file)
+
+    if samples.shape[1] > 1:
+        samples = librosa.to_mono(lib_samp)
+    
+    make_plots(samples, sample_rate, 'og')
+
+    og_samp_cols = st.columns((3,3,3))
+
+    with og_samp_cols[0]:
+        st.subheader('Original Sample: Spectrogram')
+        st.image(load_img('og_spec_axis.png'), caption='Spectrogram')
+    with og_samp_cols[1]:
+        st.subheader('Original Sample: Amplitude Plot')
+        st.image(load_img('og_adsr_axis.png'), caption='Amplitude')
+    with og_samp_cols[2]:
+        st.subheader('Original Sample')
+        st.audio(file, format="audio/wav", start_time=0)
+
+        #display.waveshow(lib_samp)
 
     st.text('Predictions')
 
-    spec_img = load_img('spec_no_axis.png')
-    adsr_img = load_img('adsr_no_axis.png', target_size=(256, 256))
+    spec_img = load_img('og_spec_no_axis.png')
+    adsr_img = load_img('og_adsr_no_axis.png', target_size=(256, 256))
     
-    img_array = img_to_array(spec_img)
-    img_batch = np.expand_dims(img_array, axis=0)
-    st.text((list(json.loads(class_labels).keys())[wv_model.predict(img_batch).argmax(-1)[0]]))
+    spec_img_array = img_to_array(spec_img)
+    spec_img_batch = np.expand_dims(spec_img_array, axis=0)
+    st.text((list(json.loads(class_labels).keys())[wv_model.predict(spec_img_batch).argmax(-1)[0]]))
 
-    img_array = img_to_array(adsr_img)
-    img_batch = np.expand_dims(img_array, axis=0)
-    adsr_estimations = adsr_model.predict(img_batch/255)[0]
+    adsr_img_array = img_to_array(adsr_img)
+    adsr_img_batch = np.expand_dims(adsr_img_array, axis=0)
+    adsr_estimations = adsr_model.predict(adsr_img_batch/255)[0]
 
     re_exp = '-?\d*\.\d{3}'
 
@@ -98,7 +134,12 @@ if file is not None:
     s_dur = re.match(re_exp, str(adsr_estimations[3]))[0]
     r_dur = re.match(re_exp, str(adsr_estimations[4]))[0]
     s_lvl = re.match(re_exp, str(adsr_estimations[2]))[0]
-    
+
+
+    _, pitches, _, _ = crepe.predict(samples[sample_rate*(int(float(a_dur))):(sample_rate*(int(float(a_dur)))+sample_rate)], sr=sample_rate)
+
+    print(librosa.hz_to_note(pitches.mean()))
+
     st.text(f'''
         Attack Duration: {a_dur}s
         Decay Duration: {d_dur}s
@@ -107,9 +148,54 @@ if file is not None:
         Sustain Level: {float(s_lvl)*100}%
         ''')
 
-    if ((list(json.loads(class_labels).keys())[wv_model.predict(img_batch).argmax(-1)[0]]) == 'sq'):
-        waveform = oscillators.SawtoothOscillator
-        
+    waveform_prediction = (list(json.loads(class_labels).keys())[wv_model.predict(spec_img_batch).argmax(-1)[0]]) 
 
+    if (waveform_prediction == 'sq'):
+        waveform = oscillators.SquareOscillator
+    elif (waveform_prediction == 'saw'):
+        waveform = oscillators.SawtoothOscillator
+    if (waveform_prediction == 'sine'):
+        waveform = oscillators.SineOscillator
+    if (waveform_prediction == 'tri'):
+        waveform = oscillators.TriangleOscillator
+
+    a_dur = float(a_dur)
+    d_dur = float(d_dur)
+    s_lvl = float(s_lvl)
+    s_dur = float(s_dur)
+    r_dur = float(r_dur)
+    
+    osc = oscillators.ModulatedOscillator(
+        waveform(freq=librosa.note_to_hz(
+            chosen_note+str(int(chosen_octave)))
+                if predict_note_octave == True
+                else pitches.mean()    
+            , amp=1),
+        envelopes.ADSREnvelope(a_dur, d_dur, s_lvl, s_dur, r_dur),
+        amp_mod= lambda init_amp, env: env * init_amp
+    )
+
+    wav = get_val(iter(osc), 44100 * 10)
+
+    wave_to_file(wav, fname="estimation.wav")
+
+    est_file = wavfile.read('estimation.wav')
+
+    make_plots(samples=est_file[1], sample_rate=est_file[0], file_name='est')
+
+    est_cols = st.columns((3, 3, 3))
+
+    with est_cols[0]:
+        st.subheader('Sample Estimation: Spectrogram')
+        st.image(load_img('est_spec_axis.png'), caption='Spectrogram')
+    with est_cols[1]:
+        st.subheader('Sample Estimation: Amplitude Plot')
+        st.image(load_img('est_adsr_axis.png'), caption='Amplitude')
+    with est_cols[2]:
+        st.subheader('Sample Estimation')
+        st.audio(open('estimation.wav', 'rb'), format="audio/wav", start_time=0)
+
+    # oscillators.ModulatedOscillator
 
     # librosa.load(file)
+
